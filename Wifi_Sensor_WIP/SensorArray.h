c9ffea5d76da3f20b4961c46ff44efdf69604e3f
zachary-sears <zears10@gmail.com>
2024-10-07 15:34:35 -0500

    Uncommented SensorArray.h code and added a private array of DistanceSensors

--- Wifi_Sensor_WIP/SensorArray.h
+++ Wifi_Sensor_WIP/SensorArray.h
@@ -1,28 +1,34 @@
-// #ifndef SensorArray_h
-// #define SensorArray_h
+#ifndef SensorArray_h
+#define SensorArray_h
 
-// #include <Arduino.h>
-// #include <DistanceSensor.h>
+#include <Arduino.h>
+#include <DistanceSensor.h>
 
-// class SensorArray {
-//   public:
-//     /*
-//      * A constructor for a sensor array of up to four ultrasonic sensors
-//      * Takes in two arrays: trigArr and echoArr. The elements of these arrays
-//      *    are meant to be assigned in pairs, i.e. trigArr[0] will be mapped
-//      *    to the same sensor as echoArr[0]
-//      *
-//      * @param trigArr - an array of trigger pin values
-//      * @param echoArr - an array of echo pin values
-//      */
-//     SensorArray(const int* trigArr, const int* echoArr);
+class SensorArray {
+  public:
+    /*
+     * A constructor for a sensor array of up to four ultrasonic sensors
+     * Takes in two arrays: trigArr and echoArr. The elements of these arrays
+     *    are meant to be assigned in pairs, i.e. trigArr[0] will be mapped
+     *    to the same sensor as echoArr[0]
+     *
+     * @param trigArr - an array of trigger pin values
+     * @param echoArr - an array of echo pin values
+     */
+    SensorArray(const int* trigArr, const int* echoArr);
     
-//     /*
-//      * Iterates through the array of sensors and measures the distances
-//      *    each sensor sees.
-//      */
-//     void measureDistances();
-//   private:
-//     const double* distances;
-//     const 
-// }
+    /*
+     * Iterates through the array of sensors and measures the distances
+     *    each sensor sees.
+     */
+    void measureDistances();
+  private:
+    /*
+     * An array to store measured distance measurements
+     */
+    double[4] distances;
+    /*
+     * An array of ultrasonic distance sensors
+     */
+    DistanceSensor[4] array;
+}
