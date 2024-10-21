a602be99062fcc26a2989473a3e255120745860a
zachary-sears <zears10@gmail.com>
2024-10-21 15:19:12 -0500

    Rewrote the SensorArray.h and .cpp files to set a spot's status based on a measured distance and get a spot's current status

--- Wifi_Sensor_WIP/SensorArray.cpp
+++ Wifi_Sensor_WIP/SensorArray.cpp
@@ -2,16 +2,43 @@
 #include "SensorArray.h"
 #include <DistanceSensor.h>
 
+// SensorArray ::SensorArray() {
+//   // int trigArr[] = {5, 7, 9, 11};
+//   // int echoArr[] = {6, 8, 10, 12};
+
+//   // for (int i = 0; i < 3; i ++) {
+//   //   pinMode(trigArr[i], OUTPUT);
+//   //   pinMode(echoArr[i], INPUT);
+
+//   //   // DistanceSensor sensor(trigArr[i], echoArr[i]);
+
+//   //   setSensors(trigArr[i], echoArr[i], i);
+//   }
+//}
+
 SensorArray ::SensorArray() {
-  int trigArr[] = {5, 7, 9, 11};
-  int echoArr[] = {6, 8, 10, 12};
-
   for (int i = 0; i < 3; i ++) {
-    pinMode(trigArr[i], OUTPUT);
-    pinMode(echoArr[i], INPUT);
-
-    DistanceSensor sensor(trigArr[i], echoArr[i]);
-
-    setSensors(sensor, i);
+    spotStatus[i] = 0;
   }
+}
+
+void SensorArray ::setStatus(DistanceSensor sensor, int statusIndex) {
+  // Serial.println("In setStatus");
+  // Serial.println(sensor.getCM());
+  int dist = sensor.getCM();
+  Serial.print("Dist: ");
+  Serial.println(dist);
+  if (dist <= 30.0) {
+    // Serial.print("Status from setStatus: ");
+    // Serial.println(spotStatus[statusIndex]);
+    spotStatus[statusIndex] = 1;  // Spot is occupied
+    // Serial.print("Status from setStatus after setting: ");
+    // Serial.println(spotStatus[statusIndex]);
+  } else {
+    spotStatus[statusIndex] = 0;  // Spot is open
+  }
+}
+
+int SensorArray ::getStatus(int statusIndex) {
+  return spotStatus[statusIndex];
 }
--- Wifi_Sensor_WIP/Wifi_Sensor_WIP.ino
+++ Wifi_Sensor_WIP/Wifi_Sensor_WIP.ino
@@ -1,78 +1,87 @@
 #include "WifiConnection.h"
+#include "SensorArray.h"
 #include "arduino_secrets.h"
 #include <DistanceSensor.h> //Library by Segilmez06
 //#include <HttpClient.h> //Library by Adrian McEwan
 
 //Distance Sensors
 //Sensor 4
-// const int S4_Trig = 5;
-// const int S4_Echo = 6;
-// DistanceSensor sensor4 = DistanceSensor(S4_Trig, S4_Echo);
+const int S4_Trig = 5;
+const int S4_Echo = 6;
+DistanceSensor sensor4 = DistanceSensor(S4_Trig, S4_Echo);
 
-// //Sensor 3
-// const int S3_Trig = 7;
-// const int S3_Echo = 8;
-// DistanceSensor sensor3 = DistanceSensor(S3_Trig, S3_Echo);
+//Sensor 3
+const int S3_Trig = 7;
+const int S3_Echo = 8;
+DistanceSensor sensor3 = DistanceSensor(S3_Trig, S3_Echo);
 
-// //Sensor 2
-// const int S2_Trig = 9;
-// const int S2_Echo = 10;
-// DistanceSensor sensor2 = DistanceSensor(S2_Trig, S2_Echo);
+//Sensor 2
+const int S2_Trig = 9;
+const int S2_Echo = 10;
+DistanceSensor sensor2 = DistanceSensor(S2_Trig, S2_Echo);
 
-// //Sensor 1
-// const int S1_Trig = 11;
-// const int S1_Echo = 12;
-// DistanceSensor sensor1 = DistanceSensor(S1_Trig, S1_Echo);
+//Sensor 1
+const int S1_Trig = 11;
+const int S1_Echo = 12;
+DistanceSensor sensor1 = DistanceSensor(S1_Trig, S1_Echo);
 
-// int distance[4];
+SensorArray sensorArray = SensorArray();
+
+int distance[4];
 
 // //WiFi vars
 // char ssid[] = SECRET_SSID;
 // WifiConnection wifiConnection(ssid);
 
 void setup() {
-  // Serial.begin(9600);
-  // delay(5000);
+  Serial.begin(9600);
+  delay(5000);
   // Serial.println("WiFi connection begin");
 
-  //Sensor 1 Pin Mode
-  // pinMode(S1_Trig, OUTPUT);
-  // pinMode(S1_Echo, INPUT);
+  // Sensor 1 Pin Mode
+  pinMode(S1_Trig, OUTPUT);
+  pinMode(S1_Echo, INPUT);
 
-  // //Sensor 2 Pin Mode
-  // pinMode(S2_Trig, OUTPUT);
-  // pinMode(S2_Echo, INPUT);
+  //Sensor 2 Pin Mode
+  pinMode(S2_Trig, OUTPUT);
+  pinMode(S2_Echo, INPUT);
   
-  // //Sensor 3 Pin Mode
-  // pinMode(S3_Trig, OUTPUT);
-  // pinMode(S3_Echo, INPUT);
+  //Sensor 3 Pin Mode
+  pinMode(S3_Trig, OUTPUT);
+  pinMode(S3_Echo, INPUT);
 
-  // //Sensor 4 Pin Mode
-  // pinMode(S4_Trig, OUTPUT);
-  // pinMode(S4_Echo, INPUT);
+  //Sensor 4 Pin Mode
+  pinMode(S4_Trig, OUTPUT);
+  pinMode(S4_Echo, INPUT);
 
   // wifiConnection.begin();
 }
 
 void loop() {
   // wifiConnection.wifiInfo();
-  // takeMeasurements();
+  takeMeasurements();
+
+  sensorArray.setStatus(sensor1, 0);
+  // Serial.print("Sensor 1 dist: ");
+  // Serial.println(/*sensor1.getCM()*/distance[0]);
+  Serial.print("Status: ");
+  Serial.println(sensorArray.getStatus(0));
 
   // Serial.print("Sensor 1: ");
   // Serial.println(distance[0]);
 }
 
-// void takeMeasurements() {
-//     //Taking measurement.
-//   distance[0] = sensor1.getCM();
-//   delay(100);
+void takeMeasurements() {
+    //Taking measurement.
+  distance[0] = sensor1.getCM();
+  delay(100);
 
-//   distance[1] = sensor2.getCM();
-//   delay(100);
+  distance[1] = sensor2.getCM();
+  delay(100);
 
-//   distance[2] = sensor3.getCM();
-//   delay(100);
+  distance[2] = sensor3.getCM();
+  delay(100);
 
-//   distance[3] = sensor4.getCM();
-//   delay(100);
-// }66
+  distance[3] = sensor4.getCM();
+  delay(100);
+}
