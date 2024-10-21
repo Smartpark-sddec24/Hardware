c4be22942f176a37747d7eea8d02f75f7495edb7
zachary-sears <zears10@gmail.com>
2024-10-21 17:26:13 -0500

    Deleted old sensor array library code

--- Wifi_Sensor_WIP/SensorArray.cpp
+++ Wifi_Sensor_WIP/SensorArray.cpp
@@ -2,20 +2,6 @@
 #include "SensorArray.h"
 #include <DistanceSensor.h>
 
-// SensorArray ::SensorArray() {
-//   // int trigArr[] = {5, 7, 9, 11};
-//   // int echoArr[] = {6, 8, 10, 12};
-
-//   // for (int i = 0; i < 3; i ++) {
-//   //   pinMode(trigArr[i], OUTPUT);
-//   //   pinMode(echoArr[i], INPUT);
-
-//   //   // DistanceSensor sensor(trigArr[i], echoArr[i]);
-
-//   //   setSensors(trigArr[i], echoArr[i], i);
-//   }
-//}
-
 SensorArray ::SensorArray() {
   for (int i = 0; i < 3; i ++) {
     spotStatus[i] = 0;
--- Wifi_Sensor_WIP/SensorArray.h
+++ Wifi_Sensor_WIP/SensorArray.h
@@ -4,67 +4,7 @@
 #include <Arduino.h>
 #include <DistanceSensor.h>
 
-/**
- * The sensor array 
- */
 class SensorArray {
-  // public:
-  //   /*
-  //    * A constructor for a sensor array of up to four ultrasonic sensors
-  //    * Takes in two arrays: trigArr and echoArr. The elements of these arrays
-  //    *    are meant to be assigned in pairs, i.e. trigArr[0] will be mapped
-  //    *    to the same sensor as echoArr[0]
-  //    */
-  //   SensorArray() {
-  //     int trigArr[] = {5, 7, 9, 11};
-  //     int echoArr[] = {6, 8, 10, 12};
-
-  //     for (int i = 0; i < 3; i ++) {
-  //       pinMode(trigArr[i], OUTPUT);
-  //       pinMode(echoArr[i], INPUT);
-
-  //   // DistanceSensor sensor(trigArr[i], echoArr[i]);
-
-  //       setSensors(trigArr[i], echoArr[i], i);
-  //     }
-  //   }
-
-  //   /*
-  //    * This method intializes
-  //    */
-  //   // void setSensors(DistanceSensor sensor, int sensorArrayIndex) {
-  //   //   this->sensorArray[sensorArrayIndex] = sensor;
-  //   // }
-
-  //   void setSensors(int trigPin, int echoPin, int sensorArrayIndex) {
-  //     // if (sensorArrayIndex >= 0) {
-  //       this->sensorArray[sensorArrayIndex] = DistanceSensor(trigPin, echoPin);
-  //     }
-
-  //   /*
-  //    * Iterates through the array of sensors and measures the distances
-  //    *    each sensor sees.
-  //    */
-  //   void measureDistances() {
-  //     for(int i = 0; i < 3; i ++) {
-  //       distances[i] = sensorArray[i].getCM();
-  //     }
-  //   }
-
-  //   double* getDistances() {
-  //     return distances;
-  //   }
-  // private:
-  //   /*
-  //    * An array to store measured distance measurements
-  //    */
-  //   double distances[4];
-
-  //   /*
-  //    * An array of ultrasonic distance sensors
-  //    */
-  //   DistanceSensor sensorArray[4];
-
   public:
     SensorArray();
 
