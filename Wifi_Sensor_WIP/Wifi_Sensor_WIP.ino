d032a1ad412a0b3883448a39d816764750e3bbe7
Kennedey Reiling <kreiling@iastate.edu>
2024-11-22 15:35:32 -0600

    cleaned up the loop function

--- Wifi_Sensor_WIP/Wifi_Sensor_WIP.ino
+++ Wifi_Sensor_WIP/Wifi_Sensor_WIP.ino
@@ -23,7 +23,7 @@ WifiConnection wifiConnection_HTTP(ssid);
 
 // static boolean toggle = true;
 // static boolean tmp = toggle;
-
+bool is_reserved[4] = { 0, 0, 0, 0 };
 volatile bool timerFlag = false;
 
 void setup() {
@@ -41,41 +41,30 @@ void setup() {
 
   // Wifi connection
   Serial.println("WiFi connection begin");
-  // wifiConnection_HTTP.begin();
+  wifiConnection_HTTP.begin();
   //Interrupt Setup
-  // interruptSetup();
+  interruptSetup();
 }
 
 void loop() {
-  bool is_reserved[4] = { 0, 0, 0, 0 };
-  bool test_status = 0;
-  bool* is_occupied_arr;
+
+  bool* is_occupied_arr = sensorArray_LED.getStatus();
   // Make a POST request and handle setting LEDs
-  // if (timerFlag) {
-  //   timerFlag = false;
-  //   is_reserved = wifiConnection_HTTP.serverUpdateSpot(/*test_status -- TODO: figure out how to */, 1);  // post request posts the sensor data to a spot
+  if (timerFlag) {
+    timerFlag = false;
+    // is_reserved = wifiConnection_HTTP.serverUpdateSpot(/*test_status -- TODO: figure out how to */, 1);  // post request posts the sensor data to a spot
+    // is_reserved = wifiConnection_HTTP.serverUpdateSpot(is_occupied_arr, /*TODO:*/{ 1, 2, 3, 4 });  // post request posts the sensor data to a spot
 
-  //   ledHandler(is_reserved);
-  // }
-
-  // Take measurements
-  for (int i = 0; i < 4; i++) {
-    char distPrint[100];
-    sprintf(distPrint, "avgDist %d:", i+1);
-    Serial.print(distPrint);
-    sensorArray_LED.setStatus(sensorArr[i], i);
-    delay(1000);
   }
 
-
-  /****************** Everything below this line is unofficial and subject to deletion in loop() ******************/
-
-  // Serial.println(sensorArray_LED.getStatus(i));  // gets the status of a spot (open=false, occupied=true)
-
-  // sensorArray_LED.setLED(i, sensorArray_LED.getStatus(i));  //sets the LED to the correct color. (open=false, occupied=true)
-
-  // is_reserved = wifiConnection_HTTP.serverUpdateSpot(test_status, 1);  // post request posts the sensor data to a spot
-  // is_reserved = wifiConnection_HTTP.serverUpdateSpot(sensorArray_LED.getStatus(), { 1, 2, 3, 4 });  // post request posts the sensor data to a spot
+  // Take measurements and print their values
+  for (int i = 0; i < 4; i++) {
+    char distPrint[100];
+    sprintf(distPrint, "avgDist %d:", i + 1);
+    Serial.print(distPrint);
+    sensorArray_LED.setStatus(sensorArr[i], i);
+    delay(1000);//Eventually delete but for troubleshooting leave
+  }
 
   is_occupied_arr = sensorArray_LED.getStatus();
   for (int i = 0; i < 4; i++) {
@@ -84,80 +73,19 @@ void loop() {
     } else if (is_occupied_arr[i]) {  //checks if our system has the spot as occupied or open
       sensorArray_LED.setLED(i, 1);   //if the spot is occupied turn red
     } else {
-      sensorArray_LED.setLED(i, 0);  //if the spot is occupied turn green
+      sensorArray_LED.setLED(i, 0);  //if the spot is open turn green
     }
-    delay(500);
+    delay(500); //Eventually delete but for troubleshooting leave
   }
 
-  if (timerFlag) {
-    // Serial.println("Timer overflow");
-    timerFlag = false;
-
-    //   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  }
-  // bool is_reserved = false;
-
-
-
-  //   // Set timer TC3 to call the TC3_Handler at 1Hz
-  //   }
-
+  /****************** Everything below this line is unofficial and subject to deletion in loop() ******************/
+  // is_reserved = wifiConnection_HTTP.serverUpdateSpot(test_status, 1);  // post request posts the sensor data to a spot
 
   // And just stop, now that we've tried a download
   // while (1)
   //   ;
-  //Set timer TC3 to call the TC3_Handler at 1Hz
-  // }
+  
 }
-
-/************************** Interrupt Setup and Handling **********************************/
-// void interruptSetup() {
-//   GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN |  // Set GCLK0 (48MHz) as clock source for timer TC3
-//                       GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TCC2_TC3;
-
-//   TC3->COUNT16.CTRLA.reg = TC_CTRLA_PRESCSYNC_PRESC |    // Wrap around on next prescaler clock
-//                            TC_CTRLA_PRESCALER_DIV1024 |  // Set prescaler to 1024, 48MHz/1024 = 46.875kHz
-//                            TC_CTRLA_WAVEGEN_MFRQ;        // Put the timer TC3 into match frequency (MFRQ) mode
-
-//   TC3->COUNT16.CC[0].reg = 9374;  // 5Hz: 48MHz /(1024 * 5Hz) - 1 = 9374
-//   while (TC3->COUNT16.STATUS.bit.SYNCBUSY && wifiConnection_HTTP.status != WL_CONNECTED)
-//     ;  // Wait for synchronization
-
-//   NVIC_SetPriority(TC3_IRQn, 0);  // Set the Nested Vector Interrupt Controller (NVIC) priority for TC3 to 0 (highest)
-//   NVIC_EnableIRQ(TC3_IRQn);       // Connect TC3 to Nested Vector Interrupt Controller (NVIC)
-
-//   TC3->COUNT16.INTENSET.reg = TC_INTENSET_OVF;  // Enable TC3 overflow interrupts
-
-//   TC3->COUNT16.CTRLA.bit.ENABLE = 1;  // Enable TC3
-
-//   while (TC3->COUNT16.STATUS.bit.SYNCBUSY)
-//     ;  // Wait for synchronization
-// }
-
-// void TC3_Handler()  // Interrupt Service Routine (ISR) for timer TC3
-// {
-//   if (TC3->COUNT16.INTFLAG.bit.OVF)  // Check for overflow (OVF) interrupt
-//   {
-//     TC3->COUNT16.INTFLAG.bit.OVF = 1;  // Clear the OVF interrupt flag
-//     // Put your timer overflow (OVF) code here...
-//     timerFlag = true;
-//   }
-// }
-
-// And just stop, now that we've tried a download
-// while (1)
-//   ;
-
-
-// void ledHandler(bool is_reserved) {
-//   if (is_reserved == 1) {          // checks if the server has the spot reserved
-//     sensorArray_LED.setLED(3, 2);  // if the spot is reserved then turn yellow
-//   } else if (/*test_status*/) {    //checks if our system has the spot as occupied or open
-//     sensorArray_LED.setLED(3, 1);  //if the spot is occupied turn red
-//   } else {
-//     sensorArray_LED.setLED(3, 0);  //if the spot is occupied turn green
-//   }
-// }
 
 /************************** Interrupt Setup and Handling **********************************/
 void interruptSetup() {
@@ -183,12 +111,12 @@ void interruptSetup() {
     ;  // Wait for synchronization
 }
 
-// void TC3_Handler()  // Interrupt Service Routine (ISR) for timer TC3
-// {
-//   if (TC3->COUNT16.INTFLAG.bit.OVF)  // Check for overflow (OVF) interrupt
-//   {
-//     TC3->COUNT16.INTFLAG.bit.OVF = 1;  // Clear the OVF interrupt flag
-//     // Put your timer overflow (OVF) code here...
-//     timerFlag = true;
-//   }
-// }
+void TC3_Handler()  // Interrupt Service Routine (ISR) for timer TC3
+{
+  if (TC3->COUNT16.INTFLAG.bit.OVF)  // Check for overflow (OVF) interrupt
+  {
+    TC3->COUNT16.INTFLAG.bit.OVF = 1;  // Clear the OVF interrupt flag
+    // Put your timer overflow (OVF) code here...
+    timerFlag = true;
+  }
+}
